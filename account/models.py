78d7670b103107f0d21623bcd41b72f8ebc614ae
kenmoh <kenneth.aremoh@gmail.com>
2021-01-22 18:20:58 +0100

    Added status field to the user model

--- account/models.py
+++ account/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+STATUS_CHOICES = (
+    ('Applicane', 'Applicant'),
+    ('Employer', 'Employer')
+)
+
+
 class User(AbstractUser):
     is_verified = models.BooleanField(default=False)
     phone_number = models.CharField(max_length=35, blank=True, null=True)
-    
+    status = models.CharField(
+        max_length=35, choices=STATUS_CHOICES)
+
     def __str__(self):
         return self.username
