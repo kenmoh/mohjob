2b88e6cdd49f32c29d90100953a78dcb362ab64d
kenmoh <kenneth.aremoh@gmail.com>
2021-01-23 20:23:46 +0100

    Added MyUserAdmin and ProfileAdmin class to the admin.py file to display specific fields

--- account/admin.py
+++ account/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import User, Profile
 from .forms import CreateUserForm
 
 
 class MyUserAdmin(UserAdmin):
-    add_form = CreateUserForm
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+
+    fieldsets = (
+        (('User'), {'fields': ('username', 'phone_number',
+                               'email', 'first_name', 'last_name', 'is_verified', 'status')}),
+        (('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser')})
+    )
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    list_display = ['user', 'state', 'location']
 
 
 admin.site.register(User, MyUserAdmin)
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from job.models import Job
 
 
 def index(request):
+
     jobs = Job.objects.all()
     context = {'jobs': jobs}
     return render(request, 'pages/index.html', context)
